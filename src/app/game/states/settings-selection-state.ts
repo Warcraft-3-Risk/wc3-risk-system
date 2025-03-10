6ddf88e1888ba489de4389874c3612c434d3dcbf
Daniel Chittenden <30878518+dtchitt@users.noreply.github.com>
2025-03-09 21:32:21 -0700

    changed modeselection to settingsselection for clarity

---
+++ src/app/game/states/settings-selection-state.ts
@@ -0,0 +1,39 @@
+import { SettingsView } from 'src/app/settings/settings-view';
+import { BaseGameState } from '../base-game-state';
+import { TimedEvent } from 'src/app/timer/timed-event';
+import { TimedEventManager } from 'src/app/timer/timed-event-manager';
+import { TimerEventType } from 'src/app/timer/timed-event-type';
+
+export class SettingSelectionState extends BaseGameState {
+	private ui: SettingsView;
+
+	public enter(): void {
+		const timerEventID: TimerEventType = 'settingsSelection';
+		const timedEventManager: TimedEventManager = TimedEventManager.getInstance();
+		let timerDuration: number = 30;
+		this.ui = new SettingsView();
+
+		timedEventManager.addEvent(
+			new TimedEvent(timerEventID, timerDuration, false, true, (remainingTime) => {
+				if (remainingTime <= 0 || !this.ui.isVisible()) {
+					timedEventManager.stopEvent('settingsSelection');
+					this.exit();
+					return;
+				}
+
+				this.ui.update(remainingTime);
+
+				print(`tick ${remainingTime}`);
+			})
+		);
+	}
+	public exit(): void {
+		print('exit');
+
+		if (this.ui.isVisible()) {
+			this.ui.hide();
+		}
+
+		this.gameStateManager.nextState();
+	}
+}
