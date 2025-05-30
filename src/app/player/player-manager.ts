4849c1c6367123ff5680576592fe651b08e5940d
Microhive <microhive@outlook.com>
2025-05-30 20:46:52 +0200

    unbanning person

--- src/app/player/player-manager.ts
+++ src/app/player/player-manager.ts
@@ -10,7 +10,6 @@ const banList: string[] = [
 	'nappa#11822', //Full screen spam
 	'keen13#2151', //Map saboteur
 	'arfan#11830', //Fake account to impersonate arfan
-	'selinace#1683', //Multi accounter - serenity
 	'serenity#13183', //Multi accounter - selinace
 	'arker#11471', //Repeated harassement
 ];
