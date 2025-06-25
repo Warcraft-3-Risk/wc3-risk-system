10a1743adc9eae6f01d55af252fffbb233559c2c
Microhive <microhive@outlook.com>
2025-06-25 23:27:36 +0200

    Adding alt tabbing warning

--- src/app/game/services/w3c-tips-service.ts
+++ src/app/game/services/w3c-tips-service.ts
@@ -1,5 +1,6 @@
 export class W3CTipsService {
 	private static tips: string[] = [
+		'Alt tabbing out of the game is not recommended!\n\nIt can cause issues with the game client and may lead to unexpected behavior such as crashes or desyncs.',
 		'Practice makes perfect!\n\nPlay regularly to improve your skills.\n\nConsider joining the Risk Discord community for tips and tricks!',
 		'Watch replays of professional players to learn new strategies.\n\nWatch matches through FloTV to see how the pros do it!',
 		'Swap out your city defenders!\n\nUse the city "Swap" button to exchange defenders between cities for better defense!',
