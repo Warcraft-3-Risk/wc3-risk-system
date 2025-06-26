7b6c1f9f26783ca19cac603e6536e9e5b7386fec
Microhive <microhive@outlook.com>
2025-06-26 22:30:32 +0200

    Improve tips

--- src/app/game/services/w3c-tips-service.ts
+++ src/app/game/services/w3c-tips-service.ts
@@ -1,14 +1,14 @@
 export class W3CTipsService {
 	private static tips: string[] = [
-		'Alt tabbing out of the game is not recommended!\n\nIt can cause issues with the game client and may lead to unexpected behavior such as crashes or desyncs.',
+		'Alt-tabbing out of the game is not recommended!\n\nIt can cause issues with the game client and may lead to unexpected behavior such as crashes or desyncs.',
 		'Practice makes perfect!\n\nPlay regularly to improve your skills.\n\nConsider joining the Risk Discord community for tips and tricks!',
 		'Watch replays of professional players to learn new strategies.\n\nWatch matches through FloTV to see how the pros do it!',
-		'Swap out your city defenders!\n\nUse the city "Swap" button to exchange defenders between cities for better defense!',
-		'Cities can be captured with a single Rifleman, if approached from outside of city range!\n\nUse this to your advantage to capture extra cities at your own risk!',
-		'Remember, 25 riflemen are always strong than a single tank!',
-		'Roarers are critical support in battles!\n\nUse them to boost your units and turn the tide of battle in your favor!',
-		'Use the -ff command to concede a match if you feel you cannot win.\n\nThis will end the match and allow you to start a new one.',
-		"Use priests to heal your units between battles and keep them in fighting shape!\n\nHealing is crucial for maintaining your army's strength.",
+		'Swap out your city defenders!\n\nUse the city "Swap" button to exchange defenders\nbetween cities for better defense!',
+		'Cities can be captured with a single rifleman\nif approached from outside of city range!\n\nUse this to your advantage to capture extra cities\nat your own risk!',
+		'Remember, 25 riflemen are always stronger\nthan a single tank!',
+		'Roarers are critical support in battles!\n\nUse them to boost your units and turn the tide\nof battle in your favor!',
+		'Use the -ff command to concede a match\nif you feel you cannot win.\n\nThis will end the match and allow you to start a new one.',
+		"Use priests to heal your units between battles\nand keep them in fighting shape!\n\nHealing is crucial for maintaining your army's strength.",
 	];
 
 	public static getRandomTip(): string {
