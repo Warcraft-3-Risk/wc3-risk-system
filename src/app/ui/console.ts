ed1b8edc5c04cc2080eadaba6198946ba91a4f8e
Microhive <microhive@outlook.com>
2025-07-12 16:04:53 +0200

    Fixed ui sync issue

--- src/app/ui/console.ts
+++ src/app/ui/console.ts
@@ -105,12 +105,19 @@ export function SetConsoleUI() {
 
 	// Disable exit button for W3C mode
 	if (W3C_MODE_ENABLED) {
+		// Get all Frames outside of local scope to prevent async issues
+		const escMenuSaveLoadContainer: framehandle = BlzGetFrameByName('EscMenuSaveLoadContainer', 0);
+		const saveGameFileEditBox: framehandle = BlzGetFrameByName('SaveGameFileEditBox', 0);
+		const exitButton: framehandle = BlzGetFrameByName('ExitButton', 0);
+		const confirmQuitQuitButton: framehandle = BlzGetFrameByName('ConfirmQuitQuitButton', 0);
+		const confirmQuitMessageText: framehandle = BlzGetFrameByName('ConfirmQuitMessageText', 0);
+
 		if (!IsPlayerObserver(GetLocalPlayer())) {
-			BlzFrameSetVisible(BlzGetFrameByName('EscMenuSaveLoadContainer', 0), false);
-			BlzFrameSetEnable(BlzGetFrameByName('SaveGameFileEditBox', 0), false);
-			BlzFrameSetVisible(BlzGetFrameByName('ExitButton', 0), false);
-			BlzFrameSetEnable(BlzGetFrameByName('ConfirmQuitQuitButton', 0), false);
-			BlzFrameSetText(BlzGetFrameByName('ConfirmQuitMessageText', 0), 'Please use Quit Mission instead.');
+			BlzFrameSetVisible(escMenuSaveLoadContainer, false);
+			BlzFrameSetEnable(saveGameFileEditBox, false);
+			BlzFrameSetVisible(exitButton, false);
+			BlzFrameSetEnable(confirmQuitQuitButton, false);
+			BlzFrameSetText(confirmQuitMessageText, 'Please use Quit Mission instead.');
 		}
 	}
 }
