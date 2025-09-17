fceac8143c18dca175afbcb63b8456e04d33f10b
Microhive <microhive@outlook.com>
2025-09-17 22:33:47 +0200

    Fixed units not removed

--- src/app/game/game-mode/utillity/remove-units.ts
+++ src/app/game/game-mode/utillity/remove-units.ts
@@ -1,10 +1,12 @@
 import { UNIT_TYPE } from 'src/app/utils/unit-types';
 import { PLAYER_SLOTS } from 'src/app/utils/utils';
 import { UnitLagManager } from '../../services/unit-lag-manager';
+import { debugPrint } from 'src/app/utils/debug-print';
 
 export function removeUnits(): void {
-	for (let i = 0; i < PLAYER_SLOTS; i++) {
+	for (let i = 0; i <= PLAYER_SLOTS; i++) {
 		const player = Player(i);
+		debugPrint(`Removing units for player ${GetPlayerName(player)} index ${i}`);
 
 		const group: group = CreateGroup();
 		GroupEnumUnitsOfPlayer(
@@ -12,9 +14,8 @@ export function removeUnits(): void {
 			player,
 			Filter(() => {
 				const unit: unit = GetFilterUnit();
-				UnitLagManager.getInstance().untrackUnit(unit);
-
-				if (!IsUnitType(unit, UNIT_TYPE.BUILDING) && !IsUnitType(unit, UNIT_TYPE.GUARD)) {
+				if (!IsUnitType(unit, UNIT_TYPE.BUILDING) && !IsUnitType(unit, UNIT_TYPE.GUARD) && !IsUnitType(unit, UNIT_TYPE.MINIMAP_INDICATOR)) {
+					UnitLagManager.getInstance().untrackUnit(unit);
 					RemoveUnit(unit);
 				}
 			})
