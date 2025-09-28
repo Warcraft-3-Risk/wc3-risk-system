968797b1a5f4b89905f78641e05f0aade4f5dcbd
Microhive <microhive@outlook.com>
2025-09-28 14:49:29 +0200

    bla

--- src/app/utils/map-info.ts
+++ src/app/utils/map-info.ts
@@ -1,6 +1,6 @@
 
 	//Do not edit - this will automatically update based on the project config.json upon building the map
 	export const MAP_NAME: string = 'Risk Europe';
-	export const MAP_VERSION: string = 'dev1';
+	export const MAP_VERSION: string = 'dev';
 	export const W3C_MODE_ENABLED: boolean = false;
   
