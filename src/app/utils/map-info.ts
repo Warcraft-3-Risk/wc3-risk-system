8531b410ea56619759c16bc671def4400edefa7d
xate <xatenev@gmail.com>
2026-01-26 02:03:37 +0100

    Fix desync for writing debug logs to file, fix observer not receiving any data in leaderboard

--- src/app/utils/map-info.ts
+++ src/app/utils/map-info.ts
@@ -1,7 +1,7 @@
 
 	//Do not edit - this will automatically update based on the project config.json upon building the map
-	export const MAP_NAME: string = 'Risk Europe';
+	export const MAP_NAME: string = 'Risk Asia';
 	export const MAP_VERSION: string = 'dev';
-	export const MAP_TYPE: string = 'europe';
+	export const MAP_TYPE: string = 'asia';
 	export const W3C_MODE_ENABLED: boolean = false;
   
