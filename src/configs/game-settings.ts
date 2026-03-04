97155f1f21f17e89cb611c447ae4d89b3bdea501
xate <xatenev@gmail.com>
2026-03-04 16:55:25 +0100

    298 | Revert game-settings.ts

--- src/configs/game-settings.ts
+++ src/configs/game-settings.ts
@@ -6,16 +6,16 @@
 export const CITIES_TO_WIN_RATIO: number = 0.6;
 
 //This is the starting gold for each player. 4 gold by default.
-export const STARTING_INCOME: number = 100;
+export const STARTING_INCOME: number = 4;
 
 //This is the starting gold for each player in Chaos promode. 50 gold by default.
 export const CHAOS_STARTING_INCOME: number = 50;
 
 //This is the starting countdown for the game. 10 by default
-export const STARTING_COUNTDOWN: number = 0;
+export const STARTING_COUNTDOWN: number = 10;
 
 //This is the duration of a turn in seconds. 60 seconds by default.
-export const TURN_DURATION_IN_SECONDS: number = 30;
+export const TURN_DURATION_IN_SECONDS: number = 60;
 
 //This is the duration of a tick in seconds. 1 second by default.
 export const TICK_DURATION_IN_SECONDS: number = 1;
@@ -30,13 +30,13 @@ export const OVERTIME_MODIFIER: number = 1;
 export const CITIES_TO_WIN_WARNING_RATIO: number = 0.7;
 
 //This represents the upper bound of cities a player starts with. Default is 22.
-export const CITIES_PER_PLAYER_UPPER_BOUND: number = 1;
+export const CITIES_PER_PLAYER_UPPER_BOUND: number = 22;
 
 //This represents the duration a player can be muted for in seconds. Default is 300 seconds.
 export const STFU_DURATION: number = 300;
 
 //This represents whether debug messages should be printed. Default is false.
-export const SHOW_DEBUG_PRINTS = true;
+export const SHOW_DEBUG_PRINTS = false;
 
 // This represents whether debug logs should be saved to a file. Default is true.
 export const SAVE_DEBUG_LOGS_TO_FILE = false;
@@ -56,7 +56,7 @@ export const CAPITALS_SELECTION_PHASE: number = 30;
 // This represents whether the game should terminate if there is only one human player left
 export const W3C_TERMINATE_IF_ALONE_HUMAN_PLAYER: boolean = true;
 
-// This represents the duration of the W3C draw vote in seconds. Default is 30 seconds.
+// This represents the duration of the W3C draw vote in seconds. Default is 120 seconds.
 export const W3C_DRAW_DURATION: number = 120;
 
 // Enable/disable emitting player statistics to W3MMD (MMD) during the match and on game end.
