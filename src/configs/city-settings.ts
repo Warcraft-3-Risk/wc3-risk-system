3418dec7bfdb5d4d066ddd7a27b20bd5ff3fd036
Eiler Poulsen <microhive@gmail.com>
2026-06-06 23:18:16 +0200

    fix: update city settings for region size and guard offsets; correct starting income value

--- src/configs/city-settings.ts
+++ src/configs/city-settings.ts
@@ -1,6 +1,6 @@
 import { CityType } from 'src/app/city/city-type';
 
 export const DefaultCityType: CityType = 'land';
-export const CityRegionSize: number = 186;
-export const CityGuardXOffSet: number = 125;
-export const CityGuardYOffSet: number = 255;
+export const CityRegionSize: number = 186 * 1.5;
+export const CityGuardXOffSet: number = 125 / 2;
+export const CityGuardYOffSet: number = 255 / 2;
